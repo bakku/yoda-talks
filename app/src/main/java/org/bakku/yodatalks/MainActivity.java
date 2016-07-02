7f4b9d8bdff99280c551dad3de6a8619ede8b363
Christian Paling <christian.paling@googlemail.com>
2016-07-02 14:40:25 +0200

    implemented basic on click listener

--- app/src/main/java/org/bakku/yodatalks/MainActivity.java
+++ app/src/main/java/org/bakku/yodatalks/MainActivity.java
@@ -2,12 +2,23 @@ package org.bakku.yodatalks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+    }
+
+    public void translate(View view) {
+        Log.d("TAG", editText.getText().toString());
     }
 }
